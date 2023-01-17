9272c952adce3787e29680608ea76ab027cb5d53
Lee Smet <lee.smet@hotmail.com>
2023-01-17 14:39:38 +0100

    Rename protocol to core
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /// Wrappers for metered connections.
 pub mod bandwidth;
 /// Protocol definitions for p2p behavior.
-pub mod protocol;
+pub mod core;
 /// Sniffers for network protocols.
 pub mod sniffer;
 /// Generic L4 (tcp) proxy, using sniffers to identify web traffic and proxy it to a registered
