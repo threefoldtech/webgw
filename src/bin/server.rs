0f0110d909b5097f679b69767d43abc5dcfc9796
Lee Smet <lee.smet@hotmail.com>
2023-01-18 17:21:09 +0100

    Make server somewhat configurable
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,9 @@
-use std::time::Duration;
+use std::{
+    net::{IpAddr, Ipv6Addr},
+    time::Duration,
+};
 
+use clap::Parser;
 use tracing::{info, Level};
 use webgw::{
     core::{CoreServer, ProtocolServer, MAX_MESSAGE_BODY_SIZE, MAX_RESPONSE_BODY_SIZE},
@@ -10,17 +14,58 @@ use webgw::{
 const PING_INTERVAL: Duration = Duration::from_secs(60);
 /// Maximum amount of connections the server accepts.
 const SERVER_MAX_CONNECTIONS: u32 = 10_000;
+/// Default IP address to bind the JSONRPC socket to, used for p2p communications.
+const DEFALT_RPC_IP: IpAddr = IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0));
+/// Default port for the JSONRPC socket, used for p2p communications.
+const DEFAULT_SERVER_RPC_PORT: u16 = 9080;
 
-/// Temporary constant server address.
-const SERVER_ADDRESS: (&str, u16) = ("::", 9080);
+/// Web gateway server
+///
+/// Command line options for the server.
+#[derive(Parser)]
+struct Opts {
+    /// IP used for the jsonrpc p2p communication. This is IP must be reachable for clients.
+    #[arg(long)]
+    rpc_ip: Option<IpAddr>,
+    /// Port used for the jsonrpc p2p communication. This is the port clients will connect on.
+    #[arg(long)]
+    rpc_port: Option<u16>,
+    /// Enable debug logging.
+    #[arg(short, long)]
+    debug: bool,
+    /// Enable trace logging. This is very verbose.
+    #[arg(short, long)]
+    trace: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Opts::parse();
+
+    let level = if args.trace {
+        Level::TRACE
+    } else if args.debug {
+        Level::DEBUG
+    } else {
+        Level::INFO
+    };
+
     tracing_subscriber::fmt()
-        .with_max_level(Level::TRACE)
+        .with_max_level(level)
         .with_ansi(true)
         .with_target(true)
         .init();
+
+    let rpc_server_ip = if let Some(ip) = args.rpc_ip {
+        ip
+    } else {
+        DEFALT_RPC_IP
+    };
+    let rpc_server_port = if let Some(port) = args.rpc_port {
+        port
+    } else {
+        DEFAULT_SERVER_RPC_PORT
+    };
 
     let proxy = Proxy::new();
     let core = CoreServer::new(proxy);
@@ -32,7 +77,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .set_id_provider(jsonrpsee::server::RandomIntegerIdProvider)
         .max_request_body_size(MAX_MESSAGE_BODY_SIZE)
         .max_response_body_size(MAX_RESPONSE_BODY_SIZE)
-        .build(SERVER_ADDRESS)
+        .build((rpc_server_ip, rpc_server_port))
         .await?
         .start(core.into_rpc())?;
 
